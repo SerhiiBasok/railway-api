41370884c27c438ebe8afd916551fb8a7922f8e0
Serhii Basok <sergeybsk19@gmail.com>
2025-11-18 18:37:04 +0200

    add: add models to admin

--- railway_app/admin.py
+++ railway_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from railway_app.models import (
+    Order,
+    Station,
+    Train,
+    Ticket,
+    Journey,
+    Crew,
+    TrainType,
+    Route,
+)
+
+admin.site.register(Order)
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(Ticket)
+admin.site.register(Journey)
+admin.site.register(Crew)
+admin.site.register(TrainType)
+admin.site.register(Route)
