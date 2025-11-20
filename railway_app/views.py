d96f379d9993794e2aeebbf438c8cbbb60724c70
Serhii Basok <sergeybsk19@gmail.com>
2025-11-20 18:38:41 +0200

    feature: add views for Station

--- railway_app/views.py
+++ railway_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from railway_app.models import Station
+from railway_app.serializers import (
+    StationSerializer,
+    StationListSerializer,
+)
+from utils.custom_mixin import BaseViewSetMethodMixin
+
+
+class StationViewSet(BaseViewSetMethodMixin, viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+    action_serializers = {
+        "list": StationListSerializer,
+        "retrieve": StationSerializer,
+        "create": StationSerializer,
+        "update": StationSerializer,
+        "partial_update": StationSerializer,
+    }
